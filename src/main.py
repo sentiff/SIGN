7e3ce0953e546bc43a6793a439196cd76dad700e
sentiff <66693737+sentiff@users.noreply.github.com>
2023-06-16 22:50:38 +0200

    updated main

--- src/main.py
+++ src/main.py
@@ -1,27 +1,10 @@
-import string
-import random
-
-
-def encode(input_text: str) -> str:
-    encoded_text: list = []
-    for x in range(len(input_text)):
-        """3rd letter in each word that has over 3 letters"""
-        current_word_len: int = random.randint(1, 10)
-        current_word: list = []
-        while True:
-            for i in range(current_word_len):
-                current_word.append(random.choice(string.ascii_letters))
-            if len(current_word) > 2:
-                current_word[2] = input_text[x]
-                encoded_text.append("".join(current_word))
-                break
-            encoded_text.append("".join(current_word))
-    return " ".join(encoded_text)
+from cipher_machine import CipherMachine
 
 
 def main() -> None:
+    cypher_machine = CipherMachine()
     input_text: str = input("text to encode:\n ")
-    output_text: str = encode(input_text)
+    output_text: str = cypher_machine.encode(input_text)
     print(f"encoded text:\n {output_text}")
 
 
