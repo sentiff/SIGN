abf9e3e35f68baddafdf794c9727759edec50a46
sentiff <66693737+sentiff@users.noreply.github.com>
2023-07-01 19:54:41 +0200

    updated type hints

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from cipher_machine import CipherMachine
 def main() -> None:
     cypher_machine = CipherMachine()
     input_text: str = input("text to encode:\n ")
-    output_text: str = cypher_machine.encode(input_text)
+    output_text: tuple[str, str] = cypher_machine.encode(input_text)
     print(f"encoded text:\n {output_text}")
     decoded_text = cypher_machine.decode(output_text)
     print(f"decoded text:\n {decoded_text}")
